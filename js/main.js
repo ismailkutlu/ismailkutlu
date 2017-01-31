a74cf4e89475a57a070dbff457114f3a0c7f83be
ismailkutlu <ismail.kutlu94@gmail.com>
2017-01-31 23:51:50 +0100

    Added skills + effect

--- js/main.js
+++ js/main.js
@@ -119,4 +119,16 @@ $(document).ready(function() {
         imageresize();
     });
 
+    $(function() {
+        $('progress').each(function() {
+            var max = $(this).val();
+            $(this).val(0).animate({
+                value: max
+            }, { duration: 2000});
+        });
+        $('em').delay(1000).animate({
+            opacity: 1,
+        }, { duration: 2000})
+    });
+
 }());
